6582647611f2499b1779a45e46b0776f338d49ae
martijnvamst <martijnvamst@hotmail.com>
2019-10-03 14:13:27 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/IgnitusGames/Project-deer-chase""
    This reverts commit 2bcd7e87185df230eff3ff6545d10491e48a2acb.

---
+++ The Dark Woods Deer Chase/Assets/VerticalPlatformTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatformTrigger : MonoBehaviour
+{
+    public VerticalPlatTrigger plat_trigger;
+    private void Awake()
+    {
+        plat_trigger = gameObject.GetComponentInParent<VerticalPlatTrigger>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+
+
+
+    {
+        if (collision.gameObject.tag == "Attack")
+
+            plat_trigger.Open();
+        Destroy(gameObject);
+
+
+    }
+}
