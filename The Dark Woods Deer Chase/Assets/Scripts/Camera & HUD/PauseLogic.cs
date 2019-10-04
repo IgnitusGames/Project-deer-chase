8d7f38af9a544b13152efd7c150f7af131e9bdfd
Ino van den Berg <0928787@hr.nl>
2019-10-04 13:48:35 +0200

    Added pause menu

---
+++ The Dark Woods Deer Chase/Assets/Scripts/Camera & HUD/PauseLogic.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseLogic : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject PauseMenuUI;
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        IsPaused = false;
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+        IsPaused = false;
+    }
+    public void ToggleCheatMode()
+    {
+        if(!GameManager.game_manager.cheat_mode_is_enabled)
+        {
+            if(GameManager.game_manager.cheat_mode_click_counter == 0)
+            {
+                GameManager.game_manager.cheat_mode_is_enabled = true;
+            }
+            else
+            {
+                GameManager.game_manager.cheat_mode_click_counter -= 1;
+            }
+        }
+        else
+        {
+            GameManager.game_manager.cheat_mode_click_counter = 4;
+            GameManager.game_manager.cheat_mode_is_enabled = false;
+        }
+        print(GameManager.game_manager.cheat_mode_is_enabled);
+    }
+}
