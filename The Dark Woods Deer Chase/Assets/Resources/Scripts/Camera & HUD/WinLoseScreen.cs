b3ed32d5f20123b1f92563d68ee040dd28e1d386
Djesco <0928787@hr.nl>
2019-12-10 22:15:51 +0100

    improved save game check

--- The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/WinLoseScreen.cs
+++ The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/WinLoseScreen.cs
@@ -10,6 +10,8 @@ public class WinLoseScreen : MonoBehaviour
     public GameObject win_screen;
     public GameObject lose_screen;
     public GameObject death_screen;
+    public string next_level_name;
+    public int current_level_index;
 
     private GameObject ui;
     private GameObject player;
@@ -32,8 +34,27 @@ public class WinLoseScreen : MonoBehaviour
     }
     public void ActivateVictoryScreen()
     {
-        //SaveData new_save = new SaveData(NewCompletedLevels(), GameManager.game_manager.tutorial_scales, GameManager.game_manager.level1_scales, GameManager.game_manager.level2_scales, false);
-        //SaveSystem.SaveProgress(new_save);
+        //player is replaying a level
+        if(this.current_level_index <= GameManager.game_manager.save.level_index)
+        {
+            //if player got more collectables
+            if(GameManager.game_manager.tutorial_scales > GameManager.game_manager.save.tutorial_scales || GameManager.game_manager.level1_scales > GameManager.game_manager.save.level1_scales || GameManager.game_manager.level2_scales > GameManager.game_manager.save.level2_scales)
+            {
+                SaveData new_save = new SaveData(GameManager.game_manager.save.level_index, GameManager.game_manager.save.next_level, GameManager.game_manager.tutorial_scales, GameManager.game_manager.level1_scales, GameManager.game_manager.level2_scales, false); ;
+                SaveSystem.SaveProgress(new_save);
+            }
+            else
+            {
+                SaveData new_save = GameManager.game_manager.save;
+                SaveSystem.SaveProgress(new_save);
+            }
+        }
+        //first play
+        else
+        {
+            SaveData new_save = new SaveData(this.current_level_index, this.next_level_name, GameManager.game_manager.tutorial_scales, GameManager.game_manager.level1_scales, GameManager.game_manager.level2_scales, false);
+            SaveSystem.SaveProgress(new_save);
+        }
         ui.SetActive(false);
         win_screen.SetActive(true);
         Time.timeScale = 0;
@@ -55,14 +76,4 @@ public class WinLoseScreen : MonoBehaviour
         death_screen.SetActive(true);
         Time.timeScale = 0;       
     }
-    //private string[] NewCompletedLevels()
-    //{
-    //    string[] result = new string[GameManager.game_manager.save.completed_levels.Length];
-    //    for(int current_level = 0; current_level < GameManager.game_manager.save.completed_levels.Length; current_level++)
-    //    {
-    //        result[current_level] = GameManager.game_manager.save.completed_levels[current_level];
-    //    }
-    //    result[result.Length - 1] = SceneManager.GetActiveScene().name;
-    //    return result;
-    //}
 }
