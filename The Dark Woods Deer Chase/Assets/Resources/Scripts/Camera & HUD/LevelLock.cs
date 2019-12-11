b450d901fb09518ee723ac59ed06e54d245f3d8e
Djesco <0928787@hr.nl>
2019-12-11 21:22:05 +0100

    Added level locking to menu

---
+++ The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/LevelLock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLock : MonoBehaviour
+{
+    //variables
+    public int required_level_index;
+
+    private bool is_locked = true;
+
+    private void Start()
+    {
+        if(GameManager.game_manager.save.level_index >= this.required_level_index)
+        {
+            this.is_locked = false;
+        }
+        print(this.is_locked);
+    }
+    public void StartThisLevel(string level_name)
+    {
+        print("klik");
+        if(!this.is_locked)
+        {
+            SceneManager.LoadScene(level_name);
+        }
+    }
+}
