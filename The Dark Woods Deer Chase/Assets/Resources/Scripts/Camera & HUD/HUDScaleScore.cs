22024aad196fcdd87b11b8a005345f06b040dd15
Djesco <0928787@hr.nl>
2019-12-16 21:25:41 +0100

    added scale score in HUD

---
+++ The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/HUDScaleScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDScaleScore : MonoBehaviour
+{
+    public Sprite[] possibleScores;
+    public Image scoreDisplay;
+
+    public int score = 0;
+
+    private void Start()
+    {
+        scoreDisplay.sprite = possibleScores[score];
+    }
+    public void updateHUDScore()
+    {
+        score++;
+        scoreDisplay.sprite = possibleScores[score];
+    }
+}
--- The Dark Woods Deer Chase/Assets/Resources/Scripts/Environment/CollectableScales.cs
+++ The Dark Woods Deer Chase/Assets/Resources/Scripts/Environment/CollectableScales.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class CollectableScales : MonoBehaviour
 {
+    private GameObject scaleScore;
+
+    private void Start()
+    {
+        scaleScore = GameObject.FindGameObjectWithTag("ScaleScore");
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player")
         {
             GameManager.game_manager.AddCollectable(SceneManager.GetActiveScene().name);
+            scaleScore.GetComponent<HUDScaleScore>().updateHUDScore();
             Destroy(this.gameObject);
         }
     }
