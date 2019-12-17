87098dddfa8a98297d5abc1259497ec39d9f69c9
Djesco <0928787@hr.nl>
2019-12-17 23:09:16 +0100

    added cheat to unlock all levels

--- The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/LevelLock.cs
+++ The Dark Woods Deer Chase/Assets/Resources/Scripts/Camera & HUD/LevelLock.cs
@@ -59,4 +59,30 @@ public class LevelLock : MonoBehaviour
         GameManager.game_manager.ResetLevelCollectables(level_name);
         SceneManager.LoadScene(level_name);
     }
+    public void Unlock()
+    {
+        this.gameObject.GetComponent<Button>().interactable = true;
+        this.lockImage.SetActive(false);
+        if (!this.isTutorial)
+        {
+            if (GameManager.game_manager.save.level_index > this.required_level_index)
+            {
+                this.emptyCrystal.GetComponent<Image>().sprite = this.crystalImage;
+            }
+            switch (level)
+            {
+                case Level.Level1:
+                    this.scaleScore.GetComponent<Image>().sprite = this.numbers[GameManager.game_manager.level1_scales];
+                    break;
+                case Level.Level2:
+                    this.scaleScore.GetComponent<Image>().sprite = this.numbers[GameManager.game_manager.level2_scales];
+                    break;
+                case Level.Level3:
+                    this.scaleScore.GetComponent<Image>().sprite = this.numbers[GameManager.game_manager.level3_scales];
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
