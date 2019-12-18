35e5e8f6acf723a458df7069fd841e935059609a
martijnvamst <martijnvamst@hotmail.com>
2019-12-18 11:25:37 +0100

    VineUpward/DownwardAnimsAdded

---
+++ The Dark Woods Deer Chase/Assets/VineUpward.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineUpward : MonoBehaviour
+{
+    private PlayerLogic player_logic;
+    public Animator animator;
+    private ParticleSystem fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_logic = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLogic>();
+        fire = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponent<Animation>().Play("VineUpwardAnim");
+        //gameObject.GetComponent<Animation>().Play("VineDie");
+        FindObjectOfType<AudioManager>().Play("VineDmg");
+        StartCoroutine(WaitForAnimation());
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        fire.Play();
+    }
+
+    public IEnumerator WaitForAnimation()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Destroy(this.gameObject);
+    }
+}
